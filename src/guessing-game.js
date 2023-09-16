3b736e511dd4f6ea50c7a1b7e5744cbfa6c92a5f
semlisa1990 <semlisa1990@gmail.com>
2023-09-16 19:14:53 +0300

    feat: solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.result = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
